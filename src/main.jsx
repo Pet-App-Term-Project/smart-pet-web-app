f685936ff77649bec01b4d235fe4d1e575c330c2
Ahmet Burak Zaloğlu <aburak1104@hotmail.com>
2023-03-01 17:11:37 +0300

    init

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import * as ReactDOM from 'react-dom/client'
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+import App from './App.jsx'
+const rootElement = document.getElementById('root')
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
 )
